6bbdf60af92cc2b0ee62ea7769d1e2d02d4aaeeb
Makisuo <minetoaster12@gmail.com>
2025-08-09 11:42:45 +0200

    use convex workos integration

--- apps/backend/convex/auth.config.ts
+++ apps/backend/convex/auth.config.ts
@@ -1,11 +1,20 @@
+const clientId = process.env.WORKOS_CLIENT_ID
+
 export default {
 	providers: [
 		{
 			type: "customJwt",
 			applicationID: "convex",
 			issuer: "https://api.workos.com",
-			jwks: `https://api.workos.com/sso/jwks/${process.env.WORKOS_CLIENT_ID}`,
+			jwks: `https://api.workos.com/sso/jwks/${clientId}`,
 			algorithm: "RS256",
+		},
+		{
+			type: "customJwt",
+			issuer: `https://api.workos.com/user_management/${clientId}`,
+			algorithm: "RS256",
+			jwks: `https://api.workos.com/sso/jwks/${clientId}`,
+			applicationID: "convex",
 		},
 	],
 }
